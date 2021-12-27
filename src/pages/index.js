4800ecaaf140056129673260906d986602cf4c81
fernandobosco <fbosco@dlocal.com>
2021-12-27 02:57:42 -0300

    new logo in index

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,13 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
-
+import img from '@site/static/img/logo.png'
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)} style={{ height: '600px' }}>
       <div className="container">
+        <img src={img} style={{ height: '200px' }} />
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
